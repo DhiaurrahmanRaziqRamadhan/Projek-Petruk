7f0fd11490c638e02949d1497f74515bac6c27bd
Muhamad Ramadhan Kamal <91411703+MRamadhanKamal@users.noreply.github.com>
2022-06-15 07:07:05 +0700

    mengubah beberapa tampilan dan membuat fungsi main

--- ProjekAkhir_Petruk.cpp
+++ ProjekAkhir_Petruk.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 int urutan = 1;
-queue <int> antrian;
+queue<int> antrian;
 
 //show queue
 void showq(queue<int> antrian_apotek){
-    queue <int> tampil = antrian_apotek;
-    cout<<"Antrian Saat Ini"<<endl;
+    queue<int> tampil = antrian_apotek;
+    cout<<"No. Antrian Sedang Dilayani"<<endl;
     if(tampil.empty()){
     	cout<<"[Belum ada antrian]"<<endl<<endl;
 	}else{
 		cout<<tampil.front()<<endl<<endl;
-		cout<<"Antrian Berikutnya"<<endl;
+		cout<<"No. Antrian Berikutnya"<<endl;
 		tampil.pop();
 		if(tampil.empty()){
 			cout<<"[Kosong]";
@@ -69,4 +69,43 @@ void empty(){
 		urutan = 1;
 		cout<<"Antrian berhasil dihilangkan"<<endl;
 	}
+}
+
+int main(){
+	int choose;
+	do{
+		showq(antrian);
+		shows(antrian);
+		cout<<"PROGRAM ANTRIAN TIKET APOTEK"<<endl;
+		cout<<"1. Menambah Antrian"<<endl;
+		cout<<"2. Antrian Berikutnya"<<endl;
+		cout<<"3. Mengosongkan Antrian"<<endl;
+		cout<<"4. Exit"<<endl;
+		cout<<endl;
+		cout<<"Pilih aksi : ";cin>>choose;;
+		
+		switch(choose){
+			case 1 :
+				push();
+				getch();
+				break;
+			case 2 :
+				pop();
+				getch();
+				break;
+			case 3:
+				empty();
+				getch();
+				break;
+			case 4:
+				cout<<"Program berakhir"<<endl;
+				getch();
+				break;
+			default:
+				cout<<"Tidak ada pilihan tersebut"<<endl;
+				getch();
+				break;
+		}
+		system("cls");
+	}while(choose != 4);
 }
